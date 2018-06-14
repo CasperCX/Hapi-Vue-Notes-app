caf4a98dfdb4a9822dcca01ac3f5396ad4960b0f
Casper Groenenberg <c.groenenberg@hotmail.com>
2018-06-14 21:14:46 +0200

    some example routes

--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,46 @@
+const noteslist = [
+    {
+        title: "something",
+    },
+    {
+        title: "else",
+    }
+];
+
+
 module.exports = [
     {
         method: 'GET',
         path: '/',
-        handler: (request, reply) => {
+        handler: (request, h) => {
             return { message: 'Hello world' }
         }
     },
     {
         method: 'GET',
-        path: '/{name}',
-        handler: (request, reply) => {
-            return { message: "Hello " + encodeURIComponent(request.params.name) }
+        path: '/notes/{id}',
+        handler: (request, h) => {
+            const id = request.params.id -1;
+            if (noteslist[id]) return (noteslist[id]);
+            return ({message: `Note with number ${id} not found`});
         }
+    },
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: (request, h) => {
+            const note = request.payload;
+            noteslist.push(note);
+            return ({message: "note created"});
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: (request, h) => {
+            const id = request.params.id -1;
+            noteslist[id] = request.payload;
+            return ({message: `note ${id} updated`});
     }
+}
 ];
